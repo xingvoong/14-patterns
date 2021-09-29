bb15d79f142ae549c415c22071c29687d885f2c6
Xing Voong <xingvoong@gmail.com>
2021-09-29 10:57:55 -0700

    implement sortedSquares with 2 pointers technique

--- sortedSquares/sortedSquares.js
+++ sortedSquares/sortedSquares.js
@@ -23,7 +23,46 @@ Follow up: squaring each element and sorting the new array is very trivial, coul
 */
 
 var sortedSquares = function(nums) {
+  var n = nums.length
+  result = new Array(n);
+  var right = n - 1
+  var left = 0
+  for(var i = result.length - 1; i > -1; i--) {
+    var toSquare = nums[right]
 
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      right -= 1
+    } else {
+      toSquare = nums[left]
+      left += 1
+    }
+    result[i] = toSquare * toSquare
+  }
+
+  return result
 };
 
+var test = function() {
+  var input = [[-3, -2, -1, 4, 5, 6], [-7,-3,2,3,11]]
+  var expected = [[1, 4, 9, 16, 25, 36 ], [4,9,9,49,121]]
+  for(var i = 0; i < input.length; i++) {
+    var actual = sortedSquares(input[i])
+    if (actual.reduce((a, b) => a && expected[i].includes[b]), false) {
+      console.log(`Wrong at input: ${i}`)
+      console.log(`Expected: ${expected[i]} but got ${actual}`)
+      return
+    }
+  }
+  console.log('Passed test')
+}
+
+test()
+
+/*
+Solution with 2 pointers
+runtime: O(n)
+a for loop that loops through the array
+space: O(n)
+the result array
+*/
 
