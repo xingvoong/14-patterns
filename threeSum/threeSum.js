f0ca627fe07f109fb561743e51d3053fb1f1ca17
Xing Voong <xingvoong@gmail.com>
2021-10-01 22:22:55 -0700

    implement 3sum solution with 2 pointers

--- threeSum/threeSum.js
+++ threeSum/threeSum.js
@@ -16,11 +16,80 @@ Example 3:
 Input: nums = [0]
 Output: []
 
+Example 4:
+Input: nums = [-4, -1, -1, 0, 1, 4]
+Output:
+
 Constraints:
 - 0 <= nums.length <= 3000
 - -10^5 <= nums[i] <= 10^5
 */
 
-var threeSum = function(nums) {
-
+var twoSum = function(i, nums, result) {
+  var left = i + 1
+  var right = nums.length - 1
+  count = 0
+  while (left < right) {
+    var sum = nums[i] + nums[left] + nums[right]
+    if (sum < 0) {
+      left += 1
+    } else if (sum > 0) {
+      right -= 1
+    } else {
+      result.push([nums[i], nums[left], nums[right]])
+      left += 1
+      right -= 1
+      // to avoid duplicate triplet, increase left
+      while (left < right && nums[left] == nums[left - 1]) {
+        left += 1
+      }
+    }
+  }
+  return result
 }
+
+var threeSum = function(nums) {
+  var n = nums.length
+  nums.sort((a, b) => a - b)
+  var result = []
+
+  for(var i = 0; i < n; i++) {
+    if (nums[i] > 0) {
+      break
+    }
+    //i == 0 so that we do not go off bound.
+    if (i == 0 || nums[i] != nums[i-1]) {
+      twoSum(i, nums, result)
+    }
+  }
+  return result
+}
+
+var test = function() {
+  var input = [[0, 1, -1, -4, -1, 2], [], [0], [0, 1, -1, -4, -1, 2, -3, 3]]
+  var expected = [[[ -1, -1, 2 ], [ -1, 0, 1 ]], [], [], [
+  [ -4, 1, 3 ],
+  [ -3, 0, 3 ],
+  [ -3, 1, 2 ],
+  [ -1, -1, 2 ],
+  [ -1, 0, 1 ]
+]]
+  for(var i = 0; i < input.length; i++) {
+    var actual = threeSum(input[i])
+    if (actual.length !== 0 && actual.reduce((a,b) => a && expected[i].includes[b]), false) {
+      console.log(`Wrong at input: ${i}`)
+      console.log(`Expected: ${expected[i]} but got ${actual}`)
+      return
+    }
+  }
+  console.log('Passed test')
+}
+
+test()
+/*
+time: O(N^2):
+twoSum is O(N), we call it N time
+space:
+O(N) for array result
+and stack for sorting.
+*/
