61a764e9071e08f80466e87b137f1cc5e90a39ee
Xing Voong <xingvoong@gmail.com>
2021-11-16 00:23:25 -0800

    checkpoint

--- firstMissingPositive/constant_space_solution.py
+++ firstMissingPositive/constant_space_solution.py
@@ -1,19 +1,22 @@
 def firstMissingPositive(nums):
-  toReturn = 1
-  for i in range(len(nums)):
-    if nums[i] > 0 and nums[i] != i+1 and nums[i] < len(nums):
-      temp = nums[i]
-      nums[i] = nums[temp - 1]
-      nums[temp - 1] = temp
-  for i in nums:
-    if i == toReturn:
-      toReturn += 1
-  print(nums)
-  return toReturn
+    n = len(nums)
+    for i in range(len(nums)):
+        # only sort in range: 1 <= i <= len(nums)
+        if nums[i] > 0 and nums[i] <= len(nums) and nums[i] != i + 1:
+            j = nums[i] - 1
+            nums[i], nums[j] = nums[j], nums[i]
+    print(nums)
+
+    # but when find for the position, I need to consider 0 and not negative number
+    for k in range(n):
+        if nums[k] >= 0 and nums[k] != k + 1:
+            return k + 1
+    return n + 1
+
 
 # def test():
-#     inputs = [[1, 2, 0], [3, 4, -1, 1], [7, 8, 9, 11, 12], [1]]
-#     expected = [3, 2, 1, 2]
+#     inputs = [[1, 2, 0], [3, 4, -1, 1], [7, 8, 9, 11, 12], [1], [-1, 4, 2, 1, 9, 10]]
+#     expected = [3, 2, 1, 2, 3]
 
 #     count = 0
 #     for i in range(len(inputs)):
@@ -29,4 +32,4 @@ def firstMissingPositive(nums):
 
 
 # test()
-print(firstMissingPositive([-1,4,2,1,9]))
+print(firstMissingPositive([-1, 4, 2, 1, 9, 10]))
