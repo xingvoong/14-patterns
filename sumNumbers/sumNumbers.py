468f0f4d14e0d68b0c5d26eeeb66d837a3e696b3
Xing Voong <xingvoong@gmail.com>
2021-12-06 11:50:38 -0800

    change variable name for easier understanding of solution

--- sumNumbers/sumNumbers.py
+++ sumNumbers/sumNumbers.py
@@ -1,4 +1,4 @@
-'''
+"""
 You are given the root of a binary tree containing digits from 0 to 9 only.
 Each root-to-leaf path in the tree represents a number.
 
@@ -16,11 +16,37 @@ C:
 - The death of the tree will node exceed 10.
 E:
 
-'''
+I need to do it in preorder (root, left, right)
+"""
 
 
 def sumNumber(root):
-  """
-  :type root: TreeNode
-  :rtype: int
-  """
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    result = 0
+
+    # use preorder traversal to find the sum
+    def findSum(root, curr_number):
+        nonlocal result
+        if root:
+            curr_number = curr_number * 10 + root.val
+            # if a node is a leaf, then update root-to-leaf sum:
+            if root.left == None and root.right == None:
+                result += curr_number
+
+            if root.left != None:
+                findSum(root.left, curr_number)
+            if root.right != None:
+                findSum(root.right, curr_number)
+
+    findSum(root, 0)
+    return result
+
+
+"""
+Time complexity: O(N) to visit each node
+Space: O(H) to keep the recursion stack, where H is a tree height.
+
+"""
