5aa646aa455c995e11d0692e2ca4ea2d32740f5e
Xing Voong <xingvoong@gmail.com>
2022-06-11 20:48:01 -0700

    init single_non_duplicate

---
+++ single_non_duplicate/single_non_duplicate.py
@@ -0,0 +1,20 @@
+'''
+You are given a sorted array consiting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appear only once:
+Your solution must run in O(logN) time and O(1) space
+
+example 1:
+input: nums = [1, 1, 2, 3, 3, 4, 4, 8, 8]
+output: 2
+
+example 2:
+input: nums = [3, 3, 7, 7, 10, 11, 11]
+output: 10
+
+the trick here is the runtime: O(logN)
+'''
+
+def singleNonDuplicate(nums):
+
+    return None
